023fbb504910ddacac886bf9d5208999bf4c0b06
Olegeg <bgpfnic@protonmail.com>
2022-02-01 23:45:54 +0300

    add reload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const board = document.querySelector(".board"),
+      wrapper = document.querySelector(".wrapper"),
       cells = Array.from(document.querySelectorAll(".cell")),
+      [btnReload, btnPlayerOne, btnPlayerTwo] = document.querySelectorAll(".btn");
       winCombas = [
         [0, 1, 2],
         [3, 4, 5],
@@ -11,10 +13,15 @@ const board = document.querySelector(".board"),
         [2, 4, 6]
       ];
 
+// CONFIG GAME
 let symbolComputer = "O",
-    symbolPlayer = "X",
-    originArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    symbolPlayer   = "X",
+    initialCells   = [],
+    message        = null;
 
+isBeginGame();
+
+// Main funciton. Here the steps of the opponents take place
 function onClickBoard(event) {
     let current = event.target.closest(".cell");
     
@@ -22,20 +29,53 @@ function onClickBoard(event) {
 
     if (!current.innerHTML) {
         current.innerHTML = symbolPlayer;
-        originArray.splice(originArray.indexOf(+current.id), 1);
-        moveComputer();
-    }
+        initialCells.splice(initialCells.indexOf(+current.id), 1);
 
-    console.log(isCheckWin());
+        if (initialCells.length) moveComputer();
+
+        isCheckWin();
+    }
 }
 
+function reloadGame() {
+    isBeginGame();
+}
+
+//  Pre-game setup
+function isBeginGame() {
+    initialCells = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    cells.forEach(item => item.innerHTML = "");
+    board.addEventListener("click", onClickBoard);
+    if (message) message.remove();
+}
+
+// Step compputer
 function moveComputer() {
-    const random = Math.floor(Math.random() * originArray.length);
-    cells[originArray[random]].innerHTML = symbolComputer;
-    originArray.splice(random, 1);
+    const random = Math.floor(Math.random() * initialCells.length);
+    cells[initialCells[random]].innerHTML = symbolComputer;
+    initialCells.splice(random, 1);
+}
+
+// Happen when the cells are over or one of the combinations is true
+function gameOver(total) {
+    let result = "draw";
+    if (total == symbolComputer) result = "win computer";
+    else if (total == symbolPlayer) result = "win player";
+
+    board.removeEventListener("click", onClickBoard);
+    createMessageTotal(result);
+}
+
+function createMessageTotal(text) {
+    message = document.createElement("div");
+    message.className = "message__total";
+    message.innerHTML = text;
+    console.log(text);
+    wrapper.append(message);
 }
 
 // TODO: Refact
+// Iterates over all combinations until it finds the true one
 function isCheckWin() {
  for (let i = 0; i < winCombas.length; i++) {
         let prev, isCheck = true;
@@ -53,11 +93,15 @@ function isCheckWin() {
             }
         }
         if (isCheck) {
-            cells.forEach(item => item.innerHTML = "");
-            originArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+            gameOver(prev);
             return;
         }
     }
+    if (!initialCells.length) {
+            gameOver();
+            return;
+        } 
 }
- 
-board.addEventListener("click", onClickBoard);
+
+
+btnReload.addEventListener("click", reloadGame);
